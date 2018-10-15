173fbcb1f9b3dda7b92a31a76926ffdaacd4bf79
Anastasiia Tikhonova <tikhonova.anastasiia@gmail.com>
2018-10-15 23:22:58 +0300

    Правит данные ClientContract

--- MindbodyAPI/structures/ClientContract.php
+++ MindbodyAPI/structures/ClientContract.php
@@ -3,6 +3,14 @@ namespace MindbodyAPI\structures;
 class ClientContract {
 	public $Action; // ActionCode
 	public $ID; // int
-	
+	public $ContractName; // string
+	public $SiteID; // int
+	public $AgreementDate; // string
+	public $OriginationLocationID; // int
+	public $StartDate; // string
+	public $EndDate; // string
+	public $AutopayStatus; // string
+	public $UpcomingAutopayEvents; // Array
+
 }
 ?>
